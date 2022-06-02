1b2c6c0630d1c0d10eed13fcd3e96a06e6497714
Tony Guo <46705969+Tg0o0@users.noreply.github.com>
2022-06-02 23:41:32 +0800

    part2b: initialise

---
+++ practices/part2-notes/src/App.js
@@ -0,0 +1,18 @@
+import Note from "./components/Note";
+import { useState } from "react";
+ims
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
