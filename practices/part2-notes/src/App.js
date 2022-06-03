699ed825112d558f0bd734c7bd31bcd2df4c3b88
Tony Guo <46705969+Tg0o0@users.noreply.github.com>
2022-06-03 15:25:30 +0800

    part2b: Filtering Displayed Elements

--- practices/part2-notes/src/App.js
+++ practices/part2-notes/src/App.js
@@ -1,16 +1,43 @@
 import Note from "./components/Note";
 import { useState } from "react";
-ims
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("a new note...");
+  const [showAll, setShowAll] = useState(true);
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    };
+    setNotes(notes.concat(noteObject));
+    setNewNote("");
+  };
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter((note) => note.important === true);
+
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map((note) => (
+        {notesToShow.map((note) => (
           <Note key={note.id} note={note} />
         ))}
       </ul>
+
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit"> save</button>
+      </form>
     </div>
   );
 };
