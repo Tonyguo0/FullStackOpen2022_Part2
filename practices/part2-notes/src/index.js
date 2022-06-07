8a9cf27ff50d11953dc354be120946edfdc48f1c
Tony Guo <46705969+Tg0o0@users.noreply.github.com>
2022-06-07 22:25:51 +0800

    part2c: axios and promises promise in a variable

--- practices/part2-notes/src/index.js
+++ practices/part2-notes/src/index.js
@@ -1,27 +1,38 @@
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import axios from "axios";
+
+const promise = axios.get("http://localhost:3001/notes");
+console.log(promise);
+
+promise.then((response) => {
+  console.log(response);
+});
+
+const promise2 = axios.get("http://localhost:3001/foobar");
+console.log(promise2);
 
 const notes = [
   {
     id: 1,
-    content: 'HTML is easy',
-    date: '2019-05-30T17:30:31.098Z',
-    important: true
+    content: "HTML is easy",
+    date: "2019-05-30T17:30:31.098Z",
+    important: true,
   },
   {
     id: 2,
-    content: 'Browser can execute only JavaScript',
-    date: '2019-05-30T18:39:34.091Z',
-    important: false
+    content: "Browser can execute only JavaScript",
+    date: "2019-05-30T18:39:34.091Z",
+    important: false,
   },
   {
     id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    date: '2019-05-30T19:20:14.298Z',
-    important: true
-  }
-]
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2019-05-30T19:20:14.298Z",
+    important: true,
+  },
+];
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <App notes={notes} />
-)
+);
