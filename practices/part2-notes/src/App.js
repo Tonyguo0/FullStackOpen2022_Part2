e1bc7ee2295cf54bbde2aa927f4f8592b1ba4579
Tony Guo <46705969+Tg0o0@users.noreply.github.com>
2022-06-13 22:00:21 +0800

    part2d:  Changing importance of Notes

--- practices/part2-notes/src/App.js
+++ practices/part2-notes/src/App.js
@@ -1,10 +1,12 @@
 import Note from "./components/Note";
 import axios from "axios";
 import { useState, useEffect } from "react";
+
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
+  const [notestoshow, setNotesToShow] = useState([]);
 
   const hooks = () => {
     console.log("effect");
@@ -38,9 +40,31 @@ const App = () => {
     setNewNote(event.target.value);
   };
 
-  const notesToShow = showAll
-    ? notes
-    : notes.filter((note) => note.important === true);
+  // ternary operator at render to make note into whatever based on showAll state is true or false by clicking on show important button
+  useEffect(() => {
+    setNotesToShow(
+      showAll ? notes : notes.filter((note) => note.important === true)
+    );
+  }, [showAll, notes]);
+
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`;
+    const note = notes.find((n) => n.id === id);
+    console.log(note);
+    const changedNote = { ...note, important: !note.important };
+    console.log(changedNote);
+    // when we use put, it already sets the note into the notes url, so when we set the state of notes
+    // if it equals to id then it should be the response date else set it to what was in notes before
+    axios.put(url, changedNote).then((response) => {
+      setNotes(
+        notes.map((notee) => {
+          console.log(`notee is: `, notee, `response data is: `, response.data);
+          return notee.id === id ? response.data : notee;
+        })
+      );
+    });
+    console.log(`Importance of ${id} needs to be toggled`);
+  };
 
   return (
     <div>
@@ -51,8 +75,13 @@ const App = () => {
         </button>
       </div>
       <ul>
-        {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
+        {notestoshow.map((note) => (
+          // Note is in component folder
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         ))}
       </ul>
 
