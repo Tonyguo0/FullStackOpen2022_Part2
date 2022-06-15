ec5bf898f608b9b6f03d44836b2a92c0aaa8813e
AzureAD\TonyGuo <tony.guo@ibm.com>
2022-06-15 13:39:30 +0800

    test commit

--- excercises/data_for_countries/src/App.js
+++ excercises/data_for_countries/src/App.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 // https://api.openweathermap.org/data/2.5/weather?q={city name},{country code}&appid={API key}
 const api_key = process.env.REACT_APP_API_KEY;
-
+ 
 const Countries = ({ countries }) => {
   const [countrynametoshow, setCountryNameToShow] = useState("");
   const [showcountry, setShowCountry] = useState(false);
