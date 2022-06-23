7a49435b65d611d0ffaa3cb86894cfc04babfbbc
Tony Guo <46705969+Tg0o0@users.noreply.github.com>
2022-06-23 23:44:04 +0800

    part 2e: Improved error message

--- practices/part2-notes/src/App.js
+++ practices/part2-notes/src/App.js
@@ -2,11 +2,20 @@ import Note from "./components/Note";
 import noteService from "./services/notes";
 import { useState, useEffect } from "react";
 
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null;
+  }
+
+  return <div className="error">{message}</div>;
+};
+
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
   const [notestoshow, setNotesToShow] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("some error happened...");
 
   // get all notes from url with useEffect hook
   const hooks = () => {
@@ -64,7 +73,12 @@ const App = () => {
         setNotes(notes.map((note) => (note.id === id ? returnedNote : note)));
       })
       .catch((error) => {
-        alert(`the note '${note.content} was already deleted from server`);
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        );
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
         setNotes(notes.filter((n) => n.id !== id));
       });
     console.log(`Importance of ${id} needs to be toggled`);
@@ -73,6 +87,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? "important" : "all"}
