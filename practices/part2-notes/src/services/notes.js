b04d6e34e125c9bce41af95294cdeb4bab62dab2
Tony Guo <46705969+Tg0o0@users.noreply.github.com>
2022-07-17 00:00:00 +0800

    part3b

--- practices/part2-notes/src/services/notes.js
+++ practices/part2-notes/src/services/notes.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 // const baseUrl = "http://localhost:3001/notes";
-const baseUrl = "https://elegant-moliere-57462.herokuapp.com/api/notes";
+const baseUrl = "/api/notes";
 
 const getAll = () => {
   const request = axios.get(baseUrl);
